b3ed11fcc6beef2a5fcdb15ab7b599ab2db33a81
OminStyle <name.cmh@gmail.com>
2014-01-25 19:16:06 -0800

    implemented randomizng texture

---
+++ Assets/Script/RandomizeTexture.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeTexture : MonoBehaviour {
+	public Texture[] textures;
+	public static string answer;
+		
+	// Use this for initialization
+	void Start () {
+		int randInt = Random.Range(1, textures.Length);
+		renderer.material.mainTexture = textures[randInt];
+		answer = textures[randInt].name;
+	}
+}
