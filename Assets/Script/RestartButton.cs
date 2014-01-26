43f7805f45d64613120c9a0376b9a2da5df32985
jasonzenanwu <wuzenan2009@hotmail.com>
2014-01-26 11:29:56 -0800

    restart button

---
+++ Assets/Script/RestartButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour {
+
+    void OnGUI(){
+		if ((GUI.Button(new Rect(80, 550, 60, 30), "Restart"))){
+			Application.LoadLevel(0);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
