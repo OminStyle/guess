f6886e79c53007909580804111830f0be2dffc70
Wein Chang <weincwc@gmail.com>
2014-01-25 17:48:24 -0800

    Sound assets
    Added sound assets and KeystrokePlayer script

---
+++ Assets/Script/KeystrokePlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeystrokePlayer : MonoBehaviour {
+	
+	public AudioClip[] ksArray;
+	public bool doPlayKeyStroke = false;
+	
+	// Use this for initialization
+	void Start () {
+		if (ksArray == null) {
+			Debug.LogError ("ksArray is null");
+		}
+		else {
+			doPlayKeyStroke = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doPlayKeyStroke && Input.anyKeyDown && 
+		    !(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2) ) ){
+			PlayKeyStroke();
+		}
+	}
+	
+	public void PlayKeyStroke() {
+		AudioSource.PlayClipAtPoint(ksArray[Random.Range (0, 3)], new Vector3(0f, 0f, 0f));
+	}
+}
