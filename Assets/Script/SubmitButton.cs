79ceeaa66a0b45b45ced44e51353602a2a2b74cb
OminStyle <name.cmh@gmail.com>
2014-01-25 17:23:33 -0800

    implemented Sumbit Button GUI

---
+++ Assets/Script/SubmitButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class submitButton : MonoBehaviour {
+
+	public Texture btnTexture;
+	void OnGUI() {
+		if (!btnTexture) {
+			Debug.LogError("Please assign a texture on the inspector");
+			return;
+		}
+
+		// button using a texture
+		//if (GUI.Button(new Rect(10, 10, 50, 50), btnTexture))
+		//	Debug.Log("Clicked the button with an image");
+		
+		if (GUI.Button(new Rect(540, 650, 50, 30), "Submit")) {
+			Debug.Log("Clicked the button with text");
+
+		}
+	}
+}
