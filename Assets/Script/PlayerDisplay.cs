ae0479e262999c667773f786425569db06693ca7
mengxixi <mengxixi@hotmail.com>
2014-01-26 12:02:59 +0800

    Merge branch 'master' of https://github.com/OminStyle/guess
    Conflicts:
    	Assets/Scenes/game.unity

---
+++ Assets/Script/PlayerDisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDisplay : MonoBehaviour {
+	
+	
+	void OnGUI () {
+
+		GUI.Label (new Rect (600, 300, 100, 100), "Player " + 2/*StateManager.playerturn*/ + " 's turn");
+	}
+
+	
+
+}
