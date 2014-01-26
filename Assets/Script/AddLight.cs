a40ff7b349d1bb15b1663ba60288d6983bad3860
OminStyle <name.cmh@gmail.com>
2014-01-25 21:46:04 -0800

    changed AddLight.cs so that user can only click on allowed space

--- Assets/Script/AddLight.cs
+++ Assets/Script/AddLight.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class AddLight : MonoBehaviour {
 	public GameObject spotlight;
+	public GameObject plane;
 
 	// Use this for initialization
 	void Start () {
@@ -13,9 +14,13 @@ public class AddLight : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			Vector3 clickPosition = Input.mousePosition;
-			clickPosition = Camera.main.ScreenToWorldPoint(clickPosition);
-			clickPosition.z = -10;
-			Instantiate( spotlight, clickPosition,Quaternion.identity);
+			Debug.Log(clickPosition.x + " " + clickPosition.y + " " + clickPosition.z);
+			if(clickPosition.x > 90 && clickPosition.x < 592 &&
+			   clickPosition.y > 108 && clickPosition.y < 616) {
+				clickPosition = Camera.main.ScreenToWorldPoint(clickPosition);
+				clickPosition.z = -10;
+				Instantiate( spotlight, clickPosition,Quaternion.identity);
+			}
 		}
 	}
 }
